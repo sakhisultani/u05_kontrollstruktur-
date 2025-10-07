8660d0fbddd2a1ce8f869a23412c5251ee0d6733
vboxuser <sakhisultani2013@gmail.com>
2025-10-07 15:04:30 +0200

    create hochhas

---
+++ auf5.07/Hochhaus2.java
@@ -0,0 +1,34 @@
+public class Hochhaus2
+
+{ 
+    public static void main (String[] args)
+    {
+      System.out.println("           /\\");
+      System.out.println("          /  \\");
+      System.out.println("         /    \\");
+      System.out.println("        /      \\");
+      System.out.println("       /        \\");
+      System.out.println("      /   (  )   \\");
+      System.out.println("     /            \\");
+      System.out.println("    /______________\\");
+      System.out.println("   /|              |\\");
+      System.out.println("    |  ___    ___  |  ");
+      System.out.println("    |  | |    | |  |  ");
+      System.out.println("    |  ---    ---  |  ");
+      System.out.println("    |  ___    ___  |  ");
+      System.out.println("    |  | |    | |  |  ");
+      System.out.println("    |  ---    ---  |  ");
+      System.out.println("    |  ___    ___  |  ");
+      System.out.println("    |  | |    | |  |  ");
+      System.out.println("    |  ---    ---  |  ");
+      System.out.println("    |  ___    ___  |  ");
+      System.out.println("    |  | |    | |  |  ");
+      System.out.println("    |  ---    ---  |  ");
+      System.out.println("    |  HOTEL  ___  |  ");
+      System.out.println("    |  |  |   | |  |  ");
+      System.out.println("    |  |  |   ---  |  ");
+      System.out.println("   ================== ");
+     }
+}
+
+
