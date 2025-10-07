e905ea571aa1967b818ed5deb88ffa10a5068e1a
vboxuser <sakhisultani2013@gmail.com>
2025-10-07 13:31:06 +0200

    create file gewinner.java

---
+++ auf5.02/Gewinner.java
@@ -0,0 +1,18 @@
+// u05_kontrollstruktur
+// auf.02
+
+public class Gewinner
+{ 
+    public static void main (String[] args)
+    { 
+        System.out.println("Der sieger fuft:");
+        System.out.println();
+
+        for(int i = 1; i <= 3; i++)
+        {
+           System.out.println("Hurra");
+         }
+     
+        System.out.println("ich habe gewonnen");
+    }
+}
