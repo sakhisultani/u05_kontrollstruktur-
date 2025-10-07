1ab2165d862f93d731c8144675dc1d8c409e117a
vboxuser <sakhisultani2013@gmail.com>
2025-10-07 14:01:30 +0200

    create erfolg.java

---
+++ auf5.04/Erfolg.java
@@ -0,0 +1,19 @@
+public class Erfolg
+
+{ 
+    public static void main (String[] args)
+    {
+       int a;
+       a = 3;
+       
+       System.out.println("Regeln fuer den erfolg:");
+       System.out.println();
+  
+       for( int i = 1; i <= a; i++)
+       {
+          System.out.println("Regel " + i + ": ");
+          System.out.println("Ueben!");
+       } 
+    }
+
+}
