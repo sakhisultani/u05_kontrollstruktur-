c0c0dad7edf99c42983af8321c05c24f43c50419
vboxuser <sakhisultani2013@gmail.com>
2025-10-07 14:38:46 +0200

    create Halbieren

---
+++ auf5.06/Halbieren.java
@@ -0,0 +1,20 @@
+public class Halbieren
+
+{ 
+    public static void main (String[] args)
+    {
+       int a;
+       int b;
+
+       a = 6;
+       b = 400;
+       
+      
+       for( int i = 1; i <= a; i++)
+       {
+          System.out.println(b);
+          b = b / 2;
+       } 
+    }
+
+}
