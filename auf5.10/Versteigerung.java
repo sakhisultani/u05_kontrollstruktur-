8fa7abf3c0e34f0e7def519c7cb1a4232afe1800
vboxuser <sakhisultani2013@gmail.com>
2025-10-08 14:18:50 +0200

    ubung aufgave schreibtishctext mit while

---
+++ auf5.10/Versteigerung.java
@@ -0,0 +1,14 @@
+public class Versteigerung
+{ 
+    public static void main (String[] args) 
+    {
+       int a;
+       a = 500;
+
+       while(a < 1000)
+       {
+          System.out.println(a + "Euro");
+          a = a + 100;
+       }
+    }
+}
