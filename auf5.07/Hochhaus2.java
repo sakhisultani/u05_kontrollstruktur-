15d9a85ccae0d93b5c7c6d9a1b3dd1a665c02e57
vboxuser <sakhisultani2013@gmail.com>
2025-10-08 09:23:14 +0200

    the corret version of the hochhaus2

--- auf5.07/Hochhaus2.java
+++ auf5.07/Hochhaus2.java
@@ -1,5 +1,4 @@
 public class Hochhaus2
-
 { 
     public static void main (String[] args)
     {
@@ -12,23 +11,18 @@ public class Hochhaus2
       System.out.println("     /            \\");
       System.out.println("    /______________\\");
       System.out.println("   /|              |\\");
-      System.out.println("    |  ___    ___  |  ");
-      System.out.println("    |  | |    | |  |  ");
-      System.out.println("    |  ---    ---  |  ");
-      System.out.println("    |  ___    ___  |  ");
-      System.out.println("    |  | |    | |  |  ");
-      System.out.println("    |  ---    ---  |  ");
-      System.out.println("    |  ___    ___  |  ");
-      System.out.println("    |  | |    | |  |  ");
-      System.out.println("    |  ---    ---  |  ");
-      System.out.println("    |  ___    ___  |  ");
-      System.out.println("    |  | |    | |  |  ");
-      System.out.println("    |  ---    ---  |  ");
+        
+      for ( int i = 0; i < 4; i++)
+      {
+         System.out.println("    |  ___    ___  |  ");
+         System.out.println("    |  | |    | |  |  ");
+         System.out.println("    |  ---    ---  |  ");
+      }
       System.out.println("    |  HOTEL  ___  |  ");
       System.out.println("    |  |  |   | |  |  ");
       System.out.println("    |  |  |   ---  |  ");
       System.out.println("   ================== ");
-     }
+    }
 }
 
 
