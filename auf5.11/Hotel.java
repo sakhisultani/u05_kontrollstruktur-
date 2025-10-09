e4d13788cdc74e8da4770e6ddd56d6387ab47671
vboxuser <sakhisultani2013@gmail.com>
2025-10-09 10:16:38 +0200

    Hotel aufgabe mit if schleife

---
+++ auf5.11/Hotel.java
@@ -0,0 +1,44 @@
+public class Hotel
+{ 
+    public static void main (String[] args)
+    {
+      int sterne = 1;
+     
+      System.out.println("           /\\");
+      System.out.println("          /  \\");
+      System.out.println("         /    \\");
+      System.out.println("        /      \\");
+      System.out.println("       /        \\");
+      System.out.println("      /   (  )   \\");
+      System.out.println("     /            \\");
+      System.out.println("    /______________\\");
+      System.out.println("   /|              |\\");
+      System.out.println("    |  ___    ___  |  ");
+      System.out.println("    |  | |    | |  |  ");
+      System.out.println("    |  ---    ---  |  ");
+
+       if(sterne == 1)
+       {
+          System.out.println("    |Absteige ___  |  ");
+       }
+       
+       if(sterne == 2)
+       {
+          System.out.println("    |Gasthaus ___  |  ");
+       }      
+
+       if(sterne == 3)
+       {
+          System.out.println("    |Hotel    ___  |  ");
+       }       
+       
+       if(sterne == 4)
+       {
+          System.out.println("    |Palast   ___  |  ");
+       } 
+
+      System.out.println("    |  |  |   | |  |  ");
+      System.out.println("    |  |  |   ---  |  ");
+      System.out.println("   ================== ");
+    }
+}
