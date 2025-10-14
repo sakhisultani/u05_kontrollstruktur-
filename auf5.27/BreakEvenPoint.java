63eda6878587a461e0364ebf857164bd26039139
vboxuser <sakhisultani2013@gmail.com>
2025-10-14 11:34:25 +0200

    auf breakevenpont

---
+++ auf5.27/BreakEvenPoint.java
@@ -0,0 +1,33 @@
+public class BreakEvenPoint
+{ 
+    public static void main (String[] args)
+   {
+      int verkaufspreisProst     =  1500;
+      int rohstoffkostenProst    = 600;
+      int arbeitskostenProst     = 400;
+      int fixkostenproMonat      = 5000;
+      int maxStZahlproMonat      =15;
+
+      System.out.println();
+      System.out.println("Break-Even-Point");
+      System.out.println("================");
+      System.out.println();
+      System.out.println("Verkaufspreis pro Stuech:      1500");
+      System.out.println("Rohstoffkosten pro Stueck:     600");
+      System.out.println("Arbeitskosten pro Stueck:      400");
+      System.out.println("Fixkosten pro Monat:           5000");
+      System.out.println("Maximale Steuckzahl pro Monat:  15");
+      System.out.println();
+
+      for(int stueckzahl = 1; stueckzahl <= maxStZahlproMonat; stueckzahl++)
+      {
+         int gesamtkosten = fixkostenproMonat + (rohstoffkostenProst + arbeitskostenProst) * stueckzahl;
+
+         int erloes = verkaufspreisProst * stueckzahl;
+        
+         int ueberschuss = erloes - gesamtkosten;
+
+         System.out.println("Stuekzahl: " + stueckzahl + "  Gesamtkosten: " + gesamtkosten + "  Erloes: " + erloes + "  Ueberschuss: " + ueberschuss);
+      }
+   }
+}
